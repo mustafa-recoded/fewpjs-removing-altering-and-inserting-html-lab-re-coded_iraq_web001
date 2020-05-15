5fa688821305ef0ec45c5c1cf36a2e80c3900d82
Mustafa Nawzat <mustafa@re-coded.com>
2020-05-15 00:59:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+/*
+
+1. Create DOM elements programmatically
+2. Add elements in the DOM
+3. Update elements using `innerHTML`
+4. Change properties on DOM nodes
+5. Remove elements from the DOM
+
+*/
+
+const p = document.createElement(p)
+document.body.appendChid(p)
+p.innerHTML = "updated for the dom"
+p.color = "red"
+p.remove()
